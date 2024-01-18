6c83b30cc6df25e625ffe039816819f0472457e1
1U23CA036ESAKKIRAJ <139437786+1U23CA036ESAKKIRAJ@users.noreply.github.com>
2024-01-18 08:44:00 +0000

    saved1

--- Person.java
+++ Person.java
@@ -1,22 +1,23 @@
 class person1{
     String firstname;
     String lastname;
-    int age;
-    person1(String fn,String ln,int a){
+    double age;
+    person1(String fn,String ln,double a){
         firstname=fn;
         lastname=ln;
         age=a;
     }
     String getfullname(){
-        return firstname+lastname;
+        return firstname+" "+lastname;
     }
 }
 public class Person {
     public static void main(String[] args) {
        person1 a1=new person1("Jhon","Doe",30);
        person1 a2=new person1("Alice","Smith",25);
-       System.out.println(a1.firstname+a1.lastname);
-  System.out.println(a2.firstname+a2.lastname);
-  System.out.println((a1.age+a2.age)/2);
+       System.out.println("Person 1: "+a1.getfullname());
+  System.out.println("Person 2: "+ a2.getfullname());
+ double b= (a1.age+a2.age)/2;
+  System.out.println("Average Age:"+b);
     }
 }
